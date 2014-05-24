3995141f0fdb91286642c3636ebf947bb00a6709
Emily Pakulski <enp2111@columbia.edu>
2014-05-24 18:55:17 -0400

    Chat is now limited to 2 people.

--- app.js
+++ app.js
@@ -21,24 +21,31 @@ require('./routes')(app, io);
 
 /************** socket io ***************/
 io.sockets.on('connection', function(clntSocket) {
-  // welcomes on succesful connection
-  clntSocket.emit('message', { message: 'Welcome to CifraChat.' });
   
-  // let current user know when new user joins
-  io.sockets.on('connection', function() {
-    clntSocket.emit('message', { message: '<b>Other</b> has joined.' });
-  });
+  // rejects connection if 2 users already present
+  if(io.sockets.clients().length <= 2)
+  {
+    // welcomes on succesful connection
+    clntSocket.emit('message', { message: 'Welcome to  CifraChat.' });
   
-  // all data sent by the user is forwarded to other users
-  clntSocket.on('send', function (text) {
-    io.sockets.emit('message', text);
-  });
+    // let current user know when new user joins
+    io.sockets.on('connection', function() {
+      clntSocket.emit('message', { message: '<b>Other</b> has joined.' });
+    });
+  
+	// all data sent by the user is forwarded to other users
+	clntSocket.on('send', function (text) {
+      io.sockets.emit('message', text);
+	});
 
-  // notify Self that somebody left the chat
-  clntSocket.on('disconnect', function() {
-	// forward to other user that this chat partner has left
-	io.sockets.emit('message', { message: '<b>Other</b> has left.' });
-	// leave the room
-	clntSocket.leave(clntSocket.room);
-  });
+	// notify Self that somebody left the chat
+	clntSocket.on('disconnect', function() {
+		// forward to other user that this chat partner has left
+		io.sockets.emit('message', { message: '<b>Other</b> has left.' });
+	});
+  }
+  else {
+	clntSocket.emit('message', { message: 'This room is full.' });
+	clntSocket.disconnect(); // force disconnect
+  };
 });
