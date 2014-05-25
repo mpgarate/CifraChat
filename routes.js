e8d396ee1ef67c257c1d4a24d18feecfa67ec5c3
Michael Garate <mpgarate@gmail.com>
2014-05-25 07:50:36 -0400

    comment routes.js

--- routes.js
+++ routes.js
@@ -1,19 +1,19 @@
 /* This file is required by app.js. 
  * It sets up the application endpoints/routes.
  * 2 routes possible:
-	  - / 				(redirect to random chat room) 
-	  - /chat/123456 	(specific chat room)
+	  - / or /chat/ or /foo				(redirect to random chat room) 
+	  - /chat/123456 							(specific chat room)
  */
 
 module.exports = function(app, io){
 
-	/* route 2: specific chat room */
+	// specific chat room request
 	app.get('/chat/:id', function(req,res){		
 		res.render('index.ejs');
 	});
 
 
-	/* route 1: redirect */
+	// any other request -> redirect to new chat room
 	app.get('/*', function(req, res){
 		// generate random chat room ID
 		var id = Math.round((Math.random() * 1000000));
