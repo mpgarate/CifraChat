70997f697922622f6c9a7c019f8baee52c40d311
Michael Garate <mpgarate@gmail.com>
2014-05-25 07:48:24 -0400

    all paths lead to chat room

--- routes.js
+++ routes.js
@@ -7,16 +7,18 @@
 
 module.exports = function(app, io){
 
+	/* route 2: specific chat room */
+	app.get('/chat/:id', function(req,res){		
+		res.render('index.ejs');
+	});
+
+
 	/* route 1: redirect */
-	app.get('/', function(req, res){
+	app.get('/*', function(req, res){
 		// generate random chat room ID
 		var id = Math.round((Math.random() * 1000000));
 
 		res.redirect('/chat/' + id);
 	});
 
-	/* route 2: specific chat room */
-	app.get('/chat/:id', function(req,res){		
-		res.render('index.ejs');
-	});
 };
